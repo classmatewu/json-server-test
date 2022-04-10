832f0f30c262dce5dc5f50304e4c5ee83588a45b
classmatewu <171755422@qq.com>
2022-04-10 17:24:32 +0800

    feat: 🌱use express to start json-server

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # json-server-test
 🍦 json2api
+
+# start 
+`pnpm init`
+`pnpm dev` or `pnpm json-server`
+`127.0.0.1:3000`
---
+++ src/app.js
@@ -0,0 +1,43 @@
+// // server.js
+// const path = require('path')
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router(path.join(__dirname, 'db.json'))
+// const middlewares = jsonServer.defaults()
+
+// server.use(middlewares)
+// server.use(router)
+// server.listen(3000, () => {
+//   console.log('JSON Server is running')
+// })
+
+const path = require('path')
+const jsonServer = require('json-server')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// Use default router
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
